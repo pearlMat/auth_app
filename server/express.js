4ad2ae801630cf0bacc4930a0ae4f0b5f175ee87
Pearl Takang <you@example.com>
2020-12-08 17:22:05 +0100

    added webpack configuration

--- server/express.js
+++ server/express.js
@@ -4,19 +4,24 @@ import cookieParser from 'cookie-parser';
 import compress from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
+import path from 'path';
+import devBundle from './devBundle';
 import Template from './../template';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 
 const app = express();
+// this is for development only!
+devBundle.compile(app);
 
-//middleware
+const CURRENT_WORKING_DIR = process.cwd(); //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 // routes
 app.use('/', authRoutes);
 app.use('/', userRoutes);
